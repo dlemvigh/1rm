68a929fa162a87e29a40613c2afb38ac0b0a1aae
David Lemvigh <dlemvigh@gmail.com>
2022-10-16 13:10:17 +0200

    Use barebones calculator

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { GlobalStyles } from './global-styles';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Calculator } from './components/1rm/Calculator';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -10,7 +11,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <GlobalStyles />
-    <App />
+    <Calculator />
+    {/* <App>
+    </App> */}
   </React.StrictMode>
 );
 
